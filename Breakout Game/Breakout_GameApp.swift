35d3cf4eb807b1ce639b545ad6eb2793456de46e
Soumyattam Dey <soumyttamdey14@gmail.com>
2022-01-04 19:44:47 +0530

    Initial Commit

---
+++ Breakout Game/Breakout_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Breakout_GameApp.swift
+//  Breakout Game
+//
+//  Created by Soumyattam Dey on 04/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Breakout_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Breakout Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Breakout Game
+//
+//  Created by Soumyattam Dey on 04/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
